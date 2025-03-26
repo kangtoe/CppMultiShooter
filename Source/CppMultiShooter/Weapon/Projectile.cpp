493a970b64f063589f049988f891a63675965878
kangtoe <kangtoe99@gmail.com>
2025-03-26 23:40:13 +0900

    RPC 방식으로 피격 효과 생성
    발사체 속도가 너무 빠른 경우, 발사체가 서버에서만 생성/삭제되고 클라이언트에서는 아예 발사체 관련 처리가 안될 수 있음. (추후 SSR 방식으로 변경 예정)

--- Source/CppMultiShooter/Weapon/Projectile.cpp
+++ Source/CppMultiShooter/Weapon/Projectile.cpp
@@ -5,6 +5,7 @@
 #include "Kismet/GameplayStatics.h"
 #include "Particles/ParticleSystemComponent.h"
 #include "Particles/ParticleSystem.h"
+#include "Sound/SoundCue.h"
 
 AProjectile::AProjectile()
 {
@@ -40,11 +41,33 @@ void AProjectile::BeginPlay()
         );
     }
 
+    if (HasAuthority())
+    {
+        CollisionBox->OnComponentHit.AddDynamic(this, &AProjectile::OnHit);
+    }
+}
+
+void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+    Destroy();
 }
 
 void AProjectile::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
+}
 
+void AProjectile::Destroyed()
+{
+    Super::Destroyed();
+
+    if (ImpactParticles)
+    {
+        UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ImpactParticles, GetActorTransform());
+    }
+    if (ImpactSound)
+    {
+        UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation());
+    }
 }
 
--- Source/CppMultiShooter/Weapon/Projectile.h
+++ Source/CppMultiShooter/Weapon/Projectile.h
@@ -15,9 +15,13 @@ class CPPMULTISHOOTER_API AProjectile : public AActor
 public:
     AProjectile();
     virtual void Tick(float DeltaTime) override;
+    virtual void Destroyed() override;
 
 protected:
     virtual void BeginPlay() override;
+
+    UFUNCTION()
+    virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 private:
 
@@ -32,6 +36,11 @@ private:
 
     class UParticleSystemComponent* TracerComponent;
 
+    UPROPERTY(EditAnywhere)
+    UParticleSystem* ImpactParticles;
+
+    UPROPERTY(EditAnywhere)
+    class USoundCue* ImpactSound;
 
 public:
 
