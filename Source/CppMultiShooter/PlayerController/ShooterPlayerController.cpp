b6853616188d66c22ce21409d9f1c561dd0d3dcd
kangtoe <kangtoe99@gmail.com>
2025-04-05 00:01:30 +0900

    서버-클라이언트 간 시간 동기화, HUD 타이머 표기
    - 서버와 클라이언트 사이의 응답 시간을 계산
    - 클라이언트는 지연 시간을 자신의 월드 시간에 더하여 서버 시간을 알아냄

--- Source/CppMultiShooter/PlayerController/ShooterPlayerController.cpp
+++ Source/CppMultiShooter/PlayerController/ShooterPlayerController.cpp
@@ -26,6 +26,14 @@ void AShooterPlayerController::OnPossess(APawn* InPawn)
     }
 }
 
+void AShooterPlayerController::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    SetHUDTime();
+    CheckTimeSync(DeltaTime);
+}
+
 void AShooterPlayerController::SetHUDScore(float Score)
 {
     ShooterHUD = ShooterHUD == nullptr ? Cast<AShooterHUD>(GetHUD()) : ShooterHUD;
@@ -79,6 +87,23 @@ void AShooterPlayerController::SetHUDCarriedAmmo(int32 Ammo)
     }
 }
 
+void AShooterPlayerController::SetHUDMatchCountdown(float CountdownTime)
+{
+    ShooterHUD = ShooterHUD == nullptr ? Cast<AShooterHUD>(GetHUD()) : ShooterHUD;
+    bool bHUDValid = ShooterHUD &&
+        ShooterHUD->CharacterOverlay &&
+        ShooterHUD->CharacterOverlay->MatchCountdownText;
+
+    if (bHUDValid)
+    {
+        int32 Minutes = FMath::FloorToInt(CountdownTime / 60.f);
+        int32 Seconds = CountdownTime - Minutes * 60;
+
+        FString CountdownText = FString::Printf(TEXT("%02d:%02d"), Minutes, Seconds);
+        ShooterHUD->CharacterOverlay->MatchCountdownText->SetText(FText::FromString(CountdownText));
+    }
+}
+
 void AShooterPlayerController::SetHUDHealth(float Health, float MaxHealth)
 {
     ShooterHUD = ShooterHUD == nullptr ? Cast<AShooterHUD>(GetHUD()) : ShooterHUD;
@@ -93,5 +118,54 @@ void AShooterPlayerController::SetHUDHealth(float Health, float MaxHealth)
         ShooterHUD->CharacterOverlay->HealthBar->SetPercent(HealthPercent);
         FString HealthText = FString::Printf(TEXT("%d/%d"), FMath::CeilToInt(Health), FMath::CeilToInt(MaxHealth));
         ShooterHUD->CharacterOverlay->HealthText->SetText(FText::FromString(HealthText));
+    }
+}
+
+void AShooterPlayerController::SetHUDTime()
+{
+    uint32 SecondsLeft = FMath::CeilToInt(MatchTime - GetServerTime());
+    if (CountdownInt != SecondsLeft)
+    {
+        SetHUDMatchCountdown(MatchTime - GetServerTime());
+    }
+
+    CountdownInt = SecondsLeft;
+}
+
+void AShooterPlayerController::CheckTimeSync(float DeltaTime)
+{
+    TimeSyncRunningTime += DeltaTime;
+    if (IsLocalController() && TimeSyncRunningTime > TimeSyncFrequency)
+    {
+        ServerRequestServerTime(GetWorld()->GetTimeSeconds());
+        TimeSyncRunningTime = 0.f;
+    }
+}
+
+void AShooterPlayerController::ServerRequestServerTime_Implementation(float TimeOfClientRequest) 
+{
+    float ServerTimeOfReceipt = GetWorld()->GetTimeSeconds(); 
+    ClientReportServerTime(TimeOfClientRequest, ServerTimeOfReceipt);
+}
+
+void AShooterPlayerController::ClientReportServerTime_Implementation(float TimeOfClientRequest, float TimeServerReceivedClientRequest)
+{
+    float RoundTripTime = GetWorld()->GetTimeSeconds() - TimeOfClientRequest;
+    float CurrentServerTime = TimeServerReceivedClientRequest + (0.5f * RoundTripTime);
+    ClientServerDelta = CurrentServerTime - GetWorld()->GetTimeSeconds();
+}
+
+float AShooterPlayerController::GetServerTime()
+{
+    if (HasAuthority()) return GetWorld()->GetTimeSeconds();
+    else return GetWorld()->GetTimeSeconds() + ClientServerDelta;
+}
+
+void AShooterPlayerController::ReceivedPlayer()
+{
+    Super::ReceivedPlayer();
+    if (IsLocalController())
+    {
+        ServerRequestServerTime(GetWorld()->GetTimeSeconds());
     }
 }
