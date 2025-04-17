9e374e0ed8ef32277387e46cab10896697e8cffc
kangtoe <kangtoe99@gmail.com>
2025-04-17 17:36:06 +0900

    로켓런처 발사 시 몽타주 소켓 수정

--- Source/CppMultiShooter/Character/ShooterCharacter.cpp
+++ Source/CppMultiShooter/Character/ShooterCharacter.cpp
@@ -149,9 +149,12 @@ void AShooterCharacter::PlayReloadMontage()
 
 		switch (Combat->EquippedWeapon->GetWeaponType())
 		{
-		case EWeaponType::EWT_AssaultRifle:
-			SectionName = FName("Rifle");
-			break;
+			case EWeaponType::EWT_AssaultRifle:
+				SectionName = FName("Rifle");
+				break;
+			case EWeaponType::EWT_RocketLauncher:
+				SectionName = FName("Rifle");
+				break;
 		}
 
 		AnimInstance->Montage_JumpToSection(SectionName);
