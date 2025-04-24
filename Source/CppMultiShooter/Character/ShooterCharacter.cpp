4dd6363cdef5fc496b0175509aa9bb2da9186d1f
kangtoe <kangtoe99@gmail.com>
2025-04-24 19:58:16 +0900

    그레네이드 런처 BP 작성

--- Source/CppMultiShooter/Character/ShooterCharacter.cpp
+++ Source/CppMultiShooter/Character/ShooterCharacter.cpp
@@ -167,7 +167,9 @@ void AShooterCharacter::PlayReloadMontage()
 			case EWeaponType::EWT_SniperRifle:
 				SectionName = FName("Rifle");
 				break;
-
+			case EWeaponType::EWT_GrenadeLauncher:
+				SectionName = FName("Rifle");
+				break;
 		}
 
 		AnimInstance->Montage_JumpToSection(SectionName);
