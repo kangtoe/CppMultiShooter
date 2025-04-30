1fb79cae69360423de321ae853de47553e0233d8
kangtoe <kangtoe99@gmail.com>
2025-05-01 07:40:56 +0900

    수류탄 투척을 서버 RPC 방식으로 수정
    - 수류탄 투척 방향이 다른 클라이언트에 동기화되지 않던 문제 수정

--- Source/CppMultiShooter/ShooterComponents/CombatComponent.cpp
+++ Source/CppMultiShooter/ShooterComponents/CombatComponent.cpp
@@ -464,10 +464,18 @@ void UCombatComponent::ThrowGrenadeFinished() // 몽타주 노티파이 이벤�
 void UCombatComponent::LaunchGrenade()
 {
 	ShowAttachedGrenade(false);
-	if (Character && Character->HasAuthority() && GrenadeClass && Character->GetAttachedGrenade())
+	if (Character && Character->IsLocallyControlled())
+	{
+		ServerLaunchGrenade(HitTarget);
+	}
+}
+
+void UCombatComponent::ServerLaunchGrenade_Implementation(const FVector_NetQuantize& Target)
+{
+	if (Character && GrenadeClass && Character->GetAttachedGrenade()) 
 	{
 		const FVector StartingLocation = Character->GetAttachedGrenade()->GetComponentLocation();
-		FVector ToTarget = HitTarget - StartingLocation;
+		FVector ToTarget = Target - StartingLocation;
 		FActorSpawnParameters SpawnParams;
 		SpawnParams.Owner = Character;
 		SpawnParams.Instigator = Character;
@@ -487,7 +495,7 @@ void UCombatComponent::LaunchGrenade()
 				Grenade->GetCollisionComponent()->IgnoreActorWhenMoving(SpawnParams.Owner, true);
 			}
 		}
-	}
+	}	
 }
 
 void UCombatComponent::ShowAttachedGrenade(bool bShowGrenade)
