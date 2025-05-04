6668ba2f9703110a78d6f7f4129691152a2c6ee1
kangtoe <kangtoe99@gmail.com>
2025-05-04 22:39:00 +0900

    픽업 위젯 로컬 표시
    - 실제 픽업 로직 처리는 서버에서만 수행하므로, 표시 로직만 로컬에서 수행

--- Source/CppMultiShooter/Weapon/Weapon.cpp
+++ Source/CppMultiShooter/Weapon/Weapon.cpp
@@ -54,14 +54,10 @@ void AWeapon::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	// 서버에서만 상호작용 영역 콜리전 처리
-	if (HasAuthority())
-	{
-		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
-		AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
-		AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnSphereOverlap);
-		AreaSphere->OnComponentEndOverlap.AddDynamic(this, &AWeapon::OnSphereEndOverlap);
-	}
+	AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnSphereOverlap);
+	AreaSphere->OnComponentEndOverlap.AddDynamic(this, &AWeapon::OnSphereEndOverlap);
 
 	if (PickupWidget)
 	{
